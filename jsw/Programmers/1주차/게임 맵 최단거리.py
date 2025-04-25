35693bd3fcc229a29112cfbba517697b5e5f7053
jangseungwon <jangseungwon9819@gmail.com>
2025-04-25 18:59:06 +0900

    제출합니다.

--- jsw/Programmers/1주차/게임 맵 최단거리.py
+++ jsw/Programmers/1주차/게임 맵 최단거리.py
@@ -1,15 +1,13 @@
 from collections import deque
 def solution(maps):
     answer = 0
-    q = deque([])
+    q = deque()
     n = len(maps)
     m = len(maps[0])
     dr = [-1,0,1,0]
     dc = [0,1,0,-1]
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 1:
-                q.append([i,j])
+    s_row, s_col = 0, 0
+    q.append([s_row,s_col])
     while q:
         length = len(q)
         for _ in range(length):
@@ -17,11 +15,14 @@ def solution(maps):
             for i in range(4):
                 nr = r + dr[i]
                 nc = c + dc[i]
-                if 0<= nr < n and 0 <= nc < m and maps[nr][nc] != 0:
-                    maps[nr][nc] += maps[r][c] +1
+                if 0 <= nr < n and 0 <= nc < m and maps[nr][nc] == 1:
+                    maps[nr][nc] = maps[r][c] + 1
+                    q.append([nr,nc])
     if maps[-1][-1] == 1:
-        print(-1)
-        return
+        answer = -1
     else:
         answer = maps[-1][-1]
     return answer
+
+회고록
+일단 BFS로 푸는 것은 깨우쳤습니다!!(하지만 다시 연습해야겠지요...)
---
+++ jsw/Programmers/2주차/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,26 @@
+-- 옛날에 푼 코드드
+SELECT A.REST_ID , A.REST_NAME , A.FOOD_TYPE , A.FAVORITES , A.ADDRESS , 
+ROUND(AVG(B.REVIEW_SCORE) , 2)
+FROM REST_INFO AS A
+JOIN REST_REVIEW AS B
+ON A.REST_ID = B.REST_ID
+WHERE A.ADDRESS LIKE '서울%'
+GROUP BY A.REST_ID
+ORDER BY AVG(B.REVIEW_SCORE) DESC , A.FAVORITES DESC
+
+--이번에 새로 푼 코드
+select i.rest_id, REST_NAME, FOOD_TYPE, FAVORITES, 
+ADDRESS, round(avg(r.review_score),2) score
+from REST_INFO  i
+left join REST_REVIEW r
+on i.rest_id = r.rest_id
+where address like '서울%' and review_score is not null
+group by i.rest_id
+order by avg(review_score) desc, favorites desc
+
+회고록
+흠...
+left join과 그냥 join의 차이점을 찾아봤는데 
+join은 교집합 left join은 합집합이라고 하는데 
+그건 join하는 키에 대해서만 그런걸로 알고있는데
+제가 잘못알고있는걸까요..? 아시는분은 좀 가르쳐 주시면 감사하겠습니다ㅎㅎ...
