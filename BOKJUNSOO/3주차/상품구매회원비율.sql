a9a17d6180873452141992e358f7fa66965f29b7
복준수 <brianbok97@gmail.com>
2025-05-17 18:09:27 +0900

    Add - [Programmers - 상품구매 회원비율]
    이번 SQL 문제도 CTE로 손쉽게 접근이 되고 풀렸다.
    한가지 주의할 점은 `JOIN`하고 나서의 결과를 보기 위해서는 duplicate에 유의.
    groupby로 집계하는것 처리 연습

---
+++ BOKJUNSOO/3주차/상품구매회원비율.sql
@@ -0,0 +1,45 @@
+WITH user AS (
+    SELECT *
+    FROM USER_INFO
+    WHERE JOINED LIKE '2021%'
+),
+joint AS (
+    SELECT
+        user.user_id,
+        sale.product_id,
+        sale.sales_amount,
+        sale.sales_date
+    FROM user
+    JOIN online_sale sale
+    ON user.user_id = sale.user_id
+),
+split AS (
+    SELECT
+        YEAR(sales_date) AS YEAR,
+        MONTH(sales_date) AS MONTH,
+        user_id,
+        sales_amount
+    FROM joint
+),
+grouped AS (
+    SELECT
+        YEAR,
+        MONTH,
+        COUNT (DISTINCT user_id) AS PURCHASED_USERS
+    FROM split
+    GROUP BY YEAR ,MONTH
+),
+finally as (
+    SELECT 
+        YEAR,
+        MONTH,
+        PURCHASED_USERS,
+        round((PURCHASED_USERS / (SELECT COUNT(*) FROM user)),1) AS PUCHASED_RATIO
+    FROM grouped
+),
+ordering as (
+    SELECT *
+    FROM finally
+    ORDER BY YEAR, MONTH
+)
+SELECT * FROM ordering;
