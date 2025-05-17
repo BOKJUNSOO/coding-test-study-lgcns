60f1ad28c892386c49c181a139bbd73ac0a97083
복준수 <brianbok97@gmail.com>
2025-05-17 18:13:27 +0900

    Add: [BOJ-1149] RGB 거리
    이번 주차의 문제중 가장 귀여운? 문제가 아니었나 싶음..
    단순히 그리디로 풀면 안되는 이유를 고민해보면 좋음

---
+++ BOKJUNSOO/3주차/RGB거리.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+cost = [[0]*3 for _ in range(n+1)]
+
+for _ in range(1,n+1):
+    new_row = list(map(int, input().split()))
+    cost[_] = new_row
+
+# dp
+dp = [[0] *3 for _ in range(n+1)]
+# init
+for j in range(3):
+    dp[1][j] = cost[1][j]
+
+# dp 탐색
+for i in range(2, n+1):
+    for j in range(3):
+        # 이전 행에서 k != j 인 값 중 최소를 직접 찾기
+        min_prev = 1001
+        for k in range(3):
+            if k == j:
+                continue
+            if dp[i-1][k] < min_prev:
+                min_prev = dp[i-1][k]
+        dp[i][j] = cost[i][j] + min_prev
+
+print(min(dp[n]))
